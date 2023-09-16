e412041eb6b6fabcc7d506443563d26e4e196346
Ahmo88 <tarevo_1988@hotmail.com>
2023-09-17 01:33:03 +0300

    add slave address

--- main/main.py
+++ main/main.py
@@ -4,22 +4,27 @@ from pymodbus.client.serial import ModbusSerialClient as ModbusClient
 
 from connect_rs485 import RS485Connection
 
-client = RS485Connection.connectRS485('rtu', 'COM3', 9600)
+client = RS485Connection.connectRS485('rtu', 'COM3', 19200)
+
+#communicate with Modbus slave ID 2 over Serial (port 0)
+slave_address = 2 
+
+
 
 
 def servo_on():
     # Implement your Modbus write operations here
-    client.write_register(33, 1)
+    client.write_register(33, 1, slave_address)
     # delay(100) NOTE: CHECK DO WE NEED  time.sleap 100ms
-    client.write_register(34, 4)
+    client.write_register(34, 4, slave_address)
     # delay(100) NOTE: CHECK DO WE NEED  time.sleap 100ms
-    client.write_register(35, 2)
+    client.write_register(35, 2, slave_address)
     # delay(100) NOTE: CHECK DO WE NEED  time.sleap 100ms
-    client.write_register(3, 1)
+    client.write_register(3, 1, slave_address)
     # delay(100) NOTE: CHECK DO WE NEED  time.sleap 100ms
-    client.write_register(40, 10000)
+    client.write_register(40, 10000, slave_address)
     # delay(100) NOTE: CHECK DO WE NEED  time.sleap 100ms
-    client.write_register(41, 10000)
+    client.write_register(41, 10000, slave_address)
     # delay(100) NOTE: CHECK DO WE NEED  time.sleap 100ms
 
 # Function to turn off the servo
@@ -27,7 +32,7 @@ def servo_on():
 
 def servo_off():
     # Implement your Modbus write operations here
-    client.write_register(3, 0)
+    client.write_register(3, 0, slave_address)
 
     pass
 
