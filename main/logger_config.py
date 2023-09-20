9bd34671a7417c9b55062b29a982c6306c8dd627
Ahmo88 <tarevo_1988@hotmail.com>
2023-09-20 16:01:02 +0300

    Added:     - logger_config     - gitignore added

---
+++ main/logger_config.py
@@ -0,0 +1,16 @@
+import logging
+
+logging.basicConfig(
+    filename='80ST-M02430-Servo.log',
+    level=logging.DEBUG,
+    format='%(asctime)s [%(levelname)s]: %(message)s',
+    datefmt='%Y-%m-%d %H:%M:%S'
+)
+
+
+# Some example log messages
+""" logging.debug("This is a debug message.")
+logging.info("This is an info message.")
+logging.warning("This is a warning message.")
+logging.error("This is an error message.")
+logging.critical("This is a critical message.") """
--- main/main.py
+++ main/main.py
@@ -1,6 +1,7 @@
 from pymodbus.client.serial import ModbusSerialClient as ModbusClient
 from connect_rs485 import RS485Connection
 import time
+from logger_config import logging
 
 # rtu is Modbus RTU communication mode
 client = RS485Connection.connectRS485('rtu', 'COM3', 19200)
@@ -21,14 +22,17 @@ def servo_off():
 
 def rotateShaft():
 
-    client.write_register(128, 50, slave_address)  # set speed
-    time.sleep(0.2)
-    # amout of rotations (if is 1 then is 1 full turn)
-    client.write_register(120, 1, slave_address)
-    time.sleep(0.1)
-    client.write_register(71, 4095, slave_address)
-    time.sleep(0.1)
-    client.write_register(71, 3071, slave_address)
+    if client:
+        client.write_register(128, 50, slave_address)  # set speed
+        time.sleep(0.2)
+        # amout of rotations (if is 1 then is 1 full turn)
+        client.write_register(120, 1, slave_address)
+        time.sleep(0.1)
+        client.write_register(71, 4095, slave_address)
+        time.sleep(0.1)
+        client.write_register(71, 3071, slave_address)
+    else:
+        logging.warning('USB-RS485 adapter is not connected. Please insert USB-RS485 adapter ')    
 
 
 def readRegister():
@@ -51,7 +55,6 @@ def readRegister():
 
 
 # servo_off()
-#servo_on()
+# servo_on()
 rotateShaft()
-readRegister()
-
+# readRegister()
