3b3798379610691f86375af7dbd9acb55ea714af
Ahmo Tokalic <tarevo_1988@hotmail.com>
2023-10-15 12:57:02 +0200

    # Set servo speed from input by button # NEXT JOB IS TO MAKE encoder.py and control_servo_template.py # TO RUN BY THREAD WITHOUT CONFLICT!! # AT A MOMENT ONNLY ONE SCRIPT CAN RUNNING!

--- main/control_servo_template.py
+++ main/control_servo_template.py
@@ -2,6 +2,7 @@ import tkinter as tk
 from tkinter import ttk
 from main_app import Servo
 from logger_config import logging
+from connect_rs485 import RS485Connection
 
 root = tk.Tk()
 root.title('Servo controlling template')
@@ -31,34 +32,42 @@ frame1.place(x=20, y=12)
 
 
 # Input fields, for full and partial turns
+# Create a label for the entry
+label = ttk.Label(root, text="Full turn:")
+label.place(x=250, y=20)
 full_turn_entry = ttk.Entry(root)
-full_turn_entry.place(x=250, y=20)
-full_turn_entry.insert(0, 'Position 1')
+full_turn_entry.place(x=250, y=40)
+full_turn_entry.insert(0, '0')
 
+label2 = ttk.Label(root, text="Partial turn:")
+label2.place(x=250, y=80)
 partial_turn_entry = ttk.Entry(root)
-partial_turn_entry.place(x=500, y=40)
-partial_turn_entry.insert(0, 'Position 2')
+partial_turn_entry.place(x=250, y=100)
+partial_turn_entry.insert(0, '0')
 
-spin = ttk.Spinbox(root, from_=0, to=100, increment=0.1)
+label3 = ttk.Label(root, text="Speed: 0~3000 r/min")
+label3.place(x=250, y=160)
+servo_speed_entry = ttk.Entry(root)
+servo_speed_entry.place(x=250, y=180)
+servo_speed_entry.insert(0, '300')
+
+client = RS485Connection.connectRS485('rtu', 'COM7', 19200)
+servo = Servo(client)
+
+button_set_speed = ttk.Button(root, text='Set Speed', 
+                    command=lambda: servo.servo_speed(speed = int(servo_speed_entry.get())))
+button_set_speed.place(x=250, y=220)
+
+""" spin = ttk.Spinbox(root, from_=0, to=100, increment=0.1)
 spin.place(x=250, y=70)
-spin.insert(0, 'Spinbox')
+spin.insert(0, 'Spinbox') """
 
 
-def rotate_chaft():
-    """ this method will get values from input fields and then 
-        will rotate servo shaft 
-    """
-    try:
-        Servo.rotate_shaft(full_turn = int(full_turn_entry.get()), partial_turn = int(partial_turn_entry.get()))
-        
-    except Exception as e:
-        # Code to handle the exception
-        logging.warning('Wrong input type for servo position')
-        logging.warning(f"An error occurred: {e}")
-        
-    
-button = ttk.Button(root, text='Rotate shaft', command=rotate_chaft)
-button.place(x=250, y=320)
+# Rotate Sertvo shaft
+button_rotate_shaft = ttk.Button(root, text='Rotate shaft', 
+                    command=lambda: servo.rotate_shaft(full_turn = int(full_turn_entry.get()), 
+                                                       partial_turn = int(partial_turn_entry.get())))
+button_rotate_shaft.place(x=250, y=320)
 
 
 def scale(i):
@@ -73,10 +82,10 @@ progress.place(x=80, y=480)
 def toggle_switch():
     """ this method will turn on/off Servo motor """
     if h.get() == 1:
-        Servo.servo_on(self=None)  # Call function when switch is ON
+        servo.servo_on()  # Call function when switch is ON
         switch.config(text='Servo ON')  # Update the text when the switch is ON
     else:
-        Servo.servo_off(self=None)  # Call function when switch is OFF
+        servo.servo_off()  # Call function when switch is OFF
         switch.config(text='Servo OFF')  # Update the text when the switch is ON
         
 switch = ttk.Checkbutton(root, text='Servo state', style='Switch', variable=h, offvalue=0, onvalue=1, command=toggle_switch)
@@ -87,6 +96,5 @@ size.place(x=780, y=510)
 
 sep1 = ttk.Separator()
 sep1.place(x=20, y=235, width=210)
-
 
 root.mainloop()
